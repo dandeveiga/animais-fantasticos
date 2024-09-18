588723e5bf6157f09961d0bc35b1e01f786390ee
Danilo Veiga <veiga.danilo@outlook.com>
2024-09-18 12:13:42 -0300

    ScrollSuave transformado em classe

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-import initSmoothScroll from "./modules/scroll-suave.js";
+import ScrollSuave from "./modules/scroll-suave.js";
 import initAnimacaoScroll from "./modules/scroll-animacao.js";
 import initAccordionList from "./modules/scroll-accordion.js";
 import initTabNav from "./modules/tabnav.js";
@@ -10,7 +10,18 @@ import initFuncionamento from "./modules/date-object.js";
 import initFetchAnimais from "./modules/fetch-animais.js";
 import initFetchBitcoin from "./modules/fetch-bitcoin.js";
 
-initSmoothScroll();
+const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
+scrollSuave.init();
+
+// se estivesse colocando options aqui, ficaria:
+// const option = {
+// behavior: 'smooth',
+// block: 'start'
+// } >> depois escreveria ,options como segundo argumento da const scrollSuave, ficaria assim:
+
+// const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]', options);
+// scrollSuave.init();
+
 initAnimacaoScroll();
 initAccordionList();
 initTabNav();
