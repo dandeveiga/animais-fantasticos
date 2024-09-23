7178715ab26b374116fd3d3ab37d245d98c4c5e1
Danilo Veiga <veiga.danilo@outlook.com>
2024-09-23 15:23:08 -0300

    anima-numeros refatorado como classe

--- js/modules/fetch-animais.js
+++ js/modules/fetch-animais.js
@@ -1,27 +1,31 @@
-import initAnimaNumeros from "./numeros.js";
+import AnimaNumeros from "./numeros.js";
 
 export default function initFetchAnimais() {
-  async function fetchAnimais(url) {
-    try {
-      const animaisResponse = await fetch(url);
-      const animaisJSON = await animaisResponse.json();
-      const numerosGrid = document.querySelector(".numeros-grid");
-
-      animaisJSON.forEach((animal) => {
-        const divAnimal = createAnimal(animal);
-        numerosGrid.appendChild(divAnimal);
-      });
-      initAnimaNumeros();
-    } catch (erro) {
-      console.log(erro("erro erro erro erro erro erro"));
-    } // poderia passar erro sem mensagem string também, ficaria apenas   console.log(erro)
-  }
-
   function createAnimal(animal) {
     const div = document.createElement("div");
     div.classList.add("numero-animal");
     div.innerHTML = `<h3>${animal.specie}</h3><span data-numero>${animal.total}</span>`;
     return div;
+  }
+
+  async function fetchAnimais(url) {
+    try {
+      const animaisResponse = await fetch(url);
+      const animaisJSON = await animaisResponse.json();
+      const numerosGrid = document.querySelector(".numeros-grid");
+      animaisJSON.forEach((animal) => {
+        const divAnimal = createAnimal(animal);
+        numerosGrid.appendChild(divAnimal);
+      });
+      const animaNumeros = new AnimaNumeros(
+        "[data-numero]",
+        ".numeros",
+        "ativo"
+      );
+      animaNumeros.init();
+    } catch (erro) {
+      console.log(erro);
+    }
   }
 
   fetchAnimais("./animaisapi.json");
--- js/modules/numeros.js
+++ js/modules/numeros.js
@@ -1,43 +1,56 @@
-export default function initAnimaNumeros() {
-  function animaNumeros() {
-    const numeros = document.querySelectorAll("[data-numero]");
+export default class AnimaNumeros {
+  constructor(numeros, observerTarget, observerClass) {
+    this.numeros = document.querySelectorAll(numeros);
+    this.observerTarget = document.querySelector(observerTarget);
+    this.observerClass = observerClass;
 
-    numeros.forEach((numero) => {
-      // para cada numero
-      const total = +numero.innerText; // localizá-los e transformar de string pra numero
-      const incremento = Math.floor(total / 100); // usar incremento para reduzir o tempo que o número corre pela tela até chegar no total, com math.floor para que ele não fique quebrado
-
-      let start = 0; // vai começar em 0
-      const timer = setInterval(() => {
-        // o setInterval vai executar até chegar no número total, parando com o clearInterval.
-        start = start + incremento;
-        numero.innerText = start;
-        if (start > total) {
-          numero.innerText = total;
-          clearInterval(timer);
-        }
-      }, 25 * Math.random()); // aqui o math.random é pra fazer com que esse número de milissegundo definido seja sempre randômico, assim, cada nova atualização de página ocorre um comportamento diferente dos números
-    });
+    // bind o this do objeto ao callback da mutação
+    this.handleMutation = this.handleMutation.bind(this);
   }
 
-  function handleMutation(mutation) {
-    if (mutation[0].target.classList.contains("ativo")) {
-      observer.disconnect();
-      animaNumeros();
+  // Recebe um elemento do dom, com número em seu texto
+  // incrementa a partir de 0 até o número final
+  static incrementarNumero(numero) {
+    const total = +numero.innerText;
+    const incremento = Math.floor(total / 100);
+    let start = 0;
+    const timer = setInterval(() => {
+      start += incremento;
+      numero.innerText = start;
+      if (start > total) {
+        numero.innerText = total;
+        clearInterval(timer);
+      }
+    }, 25 * Math.random());
+  }
+
+  // Ativa incrementar número para cada
+  // número selecionado do dom
+  animaNumeros() {
+    this.numeros.forEach((numero) =>
+      this.constructor.incrementarNumero(numero)
+    );
+  }
+
+  // Função que ocorre quando a mutaçõo ocorrer
+  handleMutation(mutation) {
+    if (mutation[0].target.classList.contains(this.observerClass)) {
+      this.observer.disconnect();
+      this.animaNumeros();
     }
   }
 
-  const observerTarget = document.querySelector(".numeros");
-  const observer = new MutationObserver(handleMutation);
+  // Adiciona o MutationObserver para verificar
+  // quanto a classe ativo é adiciona ao element target
+  addMutationObserver() {
+    this.observer = new MutationObserver(this.handleMutation);
+    this.observer.observe(this.observerTarget, { attributes: true });
+  }
 
-  observer.observe(observerTarget, { attributes: true });
-
-  // 24: usar o nome mutation no callback pois ele contém propriedades especiais de consulta no console. o handleMutation vai ser ativado quando ocorrer a mutação
-  // 25: buscando o index 0, pois é o primeiro valor de ativo que sempre aparece, criado if para que, caso o target encontre a classe ativo na mutation, executa a função animaNumeros
-  // 26: desconectar para que só anime uma vez, senão, toda vez que passar o scroll subindo ou descendo, vai recontar os números por animação
-
-  // 31: aqui é selecionado o target a ser observado, por isso a seleção da classe .numeros (do html)
-  // 32: o const observer (podia ter qualquer nome aqui) precisa ser criado com new por ser função construtor como MutationObserver, que precisa de ao menos um argumento. que vai ser o handleMutation (criado na linha 24)
-
-  // 34:  aqui é definido qual o target que o observer precisa observar, que é o que tem a classe .numeros (do html) então passa a const observerTarget, e também passa o segundo item, que vai observar também os atributos que forem true (passado como objeto).
+  init() {
+    if (this.numeros.length && this.observerTarget) {
+      this.addMutationObserver();
+    }
+    return this;
+  }
 }
